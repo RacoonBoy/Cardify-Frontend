0530f603cb3bb1ba70248377d4aeaf5984da1499
Vulong <vulong02570@gmail.com>
2024-11-17 22:37:54 +0700

    Make the Login Menu display waiting message when waiting for respond.

--- src/LoginMenu.js
+++ src/LoginMenu.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 const userURL = process.env.REACT_APP_URL+"/users"
 function LoginMenu(){
     const navigate= useNavigate()
+    const [loginText,setText] = useState("Đăng nhập")
     function login(formData){
+        setText("Đang đăng nhập ...")
         axios.post(userURL+"/login/"+formData.username.value, {
             password: formData.password.value
         })
             .then(res => {
-                sessionStorage.setItem("userId",res.data.id)
+                setText("Đăng nhập")
+                sessionStorage.setItem("userId", res.data.id)
                 navigate("/")
             })
             .catch(err => {
+                setText("Đăng nhập")
                 if(err.status===401) console.log(err.response.data)
                 else console.log(err)
             })
@@ -36,7 +41,7 @@ function LoginMenu(){
                         <input className="w-full min-w-0 px-2 py-1 border border-gray-300 hover:bg-gray-100 focus:bg-gray-100
                     focus:border-gray-500 focus:ring-1 focus:ring-gray-500 rounded-lg" type="password" name="password" required/>
                     </div>
-                    <button className="w-full px-2 py-1 mt-2 border border-gray-300 text-center rounded-lg" type="submit">Đăng nhập</button>
+                    <button id="login-button" className="w-full px-2 py-1 mt-2 border border-gray-300 text-center rounded-lg" type="submit">{loginText}</button>
                 </form>
                 <p className="mt-5 text-sm text-center font-light">
                     Nếu bạn chưa có tài khoản, hãy đăng kí tại <a href="/signup" className="font-normal text-blue-700 underline">đây</a>
